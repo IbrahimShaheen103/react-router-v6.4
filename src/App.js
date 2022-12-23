48f06e709541234df2d62a70cdaaf076e1dd53e1
IBRAHIM\Ibrahin <ishaheen910@gmail.com>
2022-12-23 20:43:20 +0200

    loaders

--- src/App.js
+++ src/App.js
@@ -9,11 +9,14 @@ import {
 // layouts
 import RootLayout from "./layouts/RootLayout";
 import HelpLayout from "./layouts/HelpLayout";
+import CareerLayout from "./layouts/CareerLayout";
 //pages
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Contact from "./pages/help/Contact";
 import Feq from "./pages/help/Feq";
+import NotFound from "./pages/NotFound";
+import Careers, { careersLoader } from "./pages/Careers";
 
 function App() {
   const router = createBrowserRouter(
@@ -27,6 +30,12 @@ function App() {
           <Route path="faq" element={<Feq />} /> {/** /help/feq */}
           <Route path="contact" element={<Contact />} /> {/** /help/contact */}
         </Route>
+        {/** loaders */}
+        <Route path="career" element={<CareerLayout />}>
+          <Route index element={<Careers />} loader={careersLoader} />
+        </Route>
+        {/**not found page */}
+        <Route path="*" element={<NotFound />} />
       </Route>
     )
   );
